23c40f91d597685d27d2e4ef1234129f60eb723f
Adel Kazem <adelkazem.1991@gmail.com>
2025-09-04 03:56:29 +0300

    grocery products

--- js/products.js
+++ js/products.js
@@ -1,49 +1,56 @@
 const PRODUCTS = [
   {
-    id: 847618708,
-    name: "Green Lion Veloura Fragrance Diffuser 22",
-    description: "The Green Lion Veloura Fragrance Diffuser has a special and modern design to fill your space with a pleasant aroma. Made from aluminum alloy and PC, it has an 800mAh battery and 8mL capacity, with an amazing 10W of power, providing up to 3 to 4 hours of continuous mist, and spreads the fragrance up to 15 square meters of space. The diffuser also features four spray modes, atmosphere lights, and touch control. These features have made this compact device a favorite among enthusiasts and people who care about life. Specification Material: Aluminum Alloy + PCFragrance Capacity: 8mLRated Power: 10WBattery Capacity: 800mAhWorking Time: 3 to 4 HoursCharging Port: Type-CInput: 5.0V/1ACharging Time: 1.5 HoursArea Coverage: 15m²FeaturesAtmosphere LightsTouch Control15m² Area Coverage4H WorkingType-C Charging0-60 C Temperature",
-    base_price: 26.93,
-    base_shipping_cost: 0,
-    free_shipping: true,
-    sku: "PRD-847618708",
-    slug: "green-lion-veloura-fragrance-diffuser-22",
-    brand: "",
-    rating: 0,
-    reviews_count: 0,
-    status: "active",
-    isFeatured: false,
-    isOnSale: false,
-    requiresShipping: true,
-    requiresInventoryTracking: true,
-    stock: 0,
-    hasVariants: false,
-    totalVariantStock: 0,
-    lowStockThreshold: 5,
-    features: {},
-    images: [
-      "products_images/GNVLRFRDFBK_Green_Lion_Veloura_Fragrance_Diffuser_1.jpg",
-      "products_images/GNVLRFRDFBK_Green_Lion_Veloura_Fragrance_Diffuser_2.jpg"
-    ],
-    options: {},
-    option_images: {},
-    variant_images: {},
-    option_variants_stock: {},
-    option_price_adjustments: {},
-    option_shipping_adjustments: {},
-    weight: {
-      value: 0,
-      unit: "kg"
-    },
-    dimensions: {
-      length: 0,
-      width: 0,
-      height: 0,
-      unit: "cm"
-    },
-    option_dimension_overrides: {},
-    categories: [],
-    createdAt: "2025-09-02T06:35:11.747Z",
-    updatedAt: "2025-09-02T06:36:00.037Z"
+    "id": 1000,
+    "name": "Krix Chips Hot & Spicy 130G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Krix_Chips_Hot_&_Spicy_130G.webp"
+  },
+  {
+    "id": 1001,
+    "name": "Krix Chips Original 130G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Krix_Chips_Original_130G.webp"
+  },
+  {
+    "id": 1002,
+    "name": "Krix Chickpeas Chips Cheese 134G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Krix_Chickpeas_Chips_Cheese_134G.webp"
+  },
+  {
+    "id": 1003,
+    "name": "Krix Chickpeas Chips Original 134G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Krix_Chickpeas_Chips_Original_134G.webp"
+  },
+  {
+    "id": 1004,
+    "name": "Krix Chickpeas Chips Sour Cream & Onion 134G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Krix_Chickpeas_Chips_Sour_Cream_&_Onion_134G.webp"
+  },
+  {
+    "id": 1005,
+    "name": "Mission Guacamole Sauce 300G",
+    "active": true,
+    "onSale": false,
+    "featured": false,
+    "stock": -1,
+    "img": "products_images/Mission_Guacamole_Sauce_300G.webp"
   }
-];
+  ];
